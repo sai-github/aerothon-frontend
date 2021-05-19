02a9d335040e8086abd01d78c7a225559267184e
sai-github <saiediitm@gmail.com>
2021-05-19 18:58:43 +0530

    feat(auth): use chakra-ui components in login and signup

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -2,6 +2,10 @@ import React from 'react';
 import axios from 'axios';
 import { Link, useHistory } from 'react-router-dom';
 import { useFormik } from 'formik';
+import { Box, Heading, VStack } from '@chakra-ui/layout';
+import { FormLabel } from '@chakra-ui/form-control';
+import { Input } from '@chakra-ui/input';
+import { Button } from '@chakra-ui/button';
 
 const validate = (values) => {
     const errors = {};
@@ -50,8 +54,10 @@ const Signup = () => {
         },
         validate,
         onSubmit: (values) => {
+            // eslint-disable-next-line no-unused-vars
+            const { retypePassword, ...filteredValues } = values;
             axios
-                .post('/api/register', values)
+                .post('/api/register', filteredValues)
                 .then((res) => {
                     console.log(res);
                     history.push('/');
@@ -62,95 +68,110 @@ const Signup = () => {
 
     return (
         <div>
-            <div>Sign up</div>
             <form onSubmit={formik.handleSubmit}>
-                <div>
-                    <label htmlFor="firstName">First Name</label>
-                    <input
-                        id="firstName"
-                        name="userFirstName"
-                        type="text"
-                        onChange={formik.handleChange}
-                        value={formik.values.userFirstName}
-                    />
-                    {formik.errors.userFirstName ? (
-                        <div>{formik.errors.userFirstName}</div>
-                    ) : null}
-                </div>
+                <Box boxShadow="md" p="6" rounded="md" bg="white">
+                    <Heading size="md" my="4">
+                        Sign up
+                    </Heading>
+                    <VStack spacing={4} align="stretch">
+                        <Box>
+                            <FormLabel htmlFor="firstName">
+                                First Name
+                            </FormLabel>
+                            <Input
+                                id="firstName"
+                                name="userFirstName"
+                                type="text"
+                                onChange={formik.handleChange}
+                                value={formik.values.userFirstName}
+                                placeholder="enter first name"
+                            />
+                            {formik.errors.userFirstName ? (
+                                <div>{formik.errors.userFirstName}</div>
+                            ) : null}
+                        </Box>
+                        <Box>
+                            <FormLabel htmlFor="firstName">Last Name</FormLabel>
+                            <Input
+                                id="lastname"
+                                name="userSecondName"
+                                type="text"
+                                onChange={formik.handleChange}
+                                value={formik.values.userSecondName}
+                                placeholder="enter last name"
+                            />
+                            {formik.errors.userSecondName ? (
+                                <div>{formik.errors.userSecondName}</div>
+                            ) : null}
+                        </Box>
+                        <Box>
+                            <FormLabel htmlFor="phoneNumber">
+                                Phone No
+                            </FormLabel>
+                            <Input
+                                id="phoneNumber"
+                                name="phoneNumber"
+                                type="string"
+                                onChange={formik.handleChange}
+                                value={formik.values.phoneNumber}
+                                placeholder="phone number"
+                            />
+                            {formik.errors.phoneNumber ? (
+                                <div>{formik.errors.phoneNumber}</div>
+                            ) : null}
+                        </Box>
+                        <Box>
+                            <FormLabel htmlFor="email">Email</FormLabel>
+                            <Input
+                                id="email"
+                                name="email"
+                                type="text"
+                                onChange={formik.handleChange}
+                                value={formik.values.email}
+                                placeholder="enter email"
+                            />
+                            {formik.errors.email ? (
+                                <div>{formik.errors.email}</div>
+                            ) : null}
+                        </Box>
+                        <Box>
+                            <FormLabel htmlFor="password">Password</FormLabel>
+                            <Input
+                                id="password"
+                                name="password"
+                                type="password"
+                                onChange={formik.handleChange}
+                                value={formik.values.password}
+                                placeholder="enter password"
+                            />
+                            {formik.errors.password ? (
+                                <div>{formik.errors.password}</div>
+                            ) : null}
+                        </Box>
+                        <Box>
+                            <FormLabel htmlFor="rePassword">
+                                Retype password
+                            </FormLabel>
+                            <Input
+                                id="rePassword"
+                                name="retypePassword"
+                                type="password"
+                                onChange={formik.handleChange}
+                                value={formik.values.retypePassword}
+                                placeholder="retype password"
+                            />
+                            {formik.errors.retypePassword ? (
+                                <div>{formik.errors.retypePassword}</div>
+                            ) : null}
+                        </Box>
 
-                <div>
-                    <label htmlFor="lastname">Last Name</label>
-                    <input
-                        id="lastname"
-                        name="userSecondName"
-                        type="text"
-                        onChange={formik.handleChange}
-                        value={formik.values.userSecondName}
-                    />
-                    {formik.errors.userSecondName ? (
-                        <div>{formik.errors.userSecondName}</div>
-                    ) : null}
-                </div>
+                        <Link to="/login">Go Login</Link>
 
-                <div>
-                    <label htmlFor="phoneNumber">Phone No</label>
-                    <input
-                        id="phoneNumber"
-                        name="phoneNumber"
-                        type="number"
-                        onChange={formik.handleChange}
-                        value={formik.values.phoneNumber}
-                    />
-                    {formik.errors.phoneNumber ? (
-                        <div>{formik.errors.phoneNumber}</div>
-                    ) : null}
-                </div>
-
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        id="email"
-                        name="email"
-                        type="text"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                    />
-                    {formik.errors.email ? (
-                        <div>{formik.errors.email}</div>
-                    ) : null}
-                </div>
-
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                    />
-                    {formik.errors.password ? (
-                        <div>{formik.errors.password}</div>
-                    ) : null}
-                </div>
-
-                <div>
-                    <label htmlFor="rePassword">Retype Password</label>
-                    <input
-                        id="rePassword"
-                        name="retypePassword"
-                        type="password"
-                        onChange={formik.handleChange}
-                        value={formik.values.retypePassword}
-                    />
-                    {formik.errors.retypePassword ? (
-                        <div>{formik.errors.retypePassword}</div>
-                    ) : null}
-                </div>
-
-                <Link to="/login">Go Login</Link>
-
-                <button type="submit">Submit</button>
+                        <Button colorScheme="teal" size="md" type="submit">
+                            Sign up
+                        </Button>
+                    </VStack>
+                </Box>
             </form>
         </div>
     );
